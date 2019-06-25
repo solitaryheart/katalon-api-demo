79e6cb073bfbdc635222cb71bbd75f28b5e16e5d
solitaryheart <matthes.meier@unitybox.de>
2019-06-25 09:37:44 +0200

    First Commit

---
+++ Test Suites/KatalonAnalytics-API.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatalonAnalytics-API</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e0015b76-3f37-4b83-a16d-5c68b15489da</testSuiteGuid>
+   <testCaseLink>
+      <guid>ab347a83-cbe6-4a79-8688-c2522b5159ec</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b6f442c-08dd-4aee-a2af-cacb41355e20</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/TestHTTPMethods</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
